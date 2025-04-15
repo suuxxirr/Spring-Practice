a864438793fd2996377aa058a2bd48d02b410a50
suuxxirr <102400242+suuxxirr@users.noreply.github.com>
2025-04-15 14:36:38 +0900

    스프링 컨테이너 & 스프링 빈

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,23 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appconfig = new AppConfig();
+        MemberService memberService1 = appconfig.memberService();
+        MemberService memberService2 = appconfig.memberService();
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
